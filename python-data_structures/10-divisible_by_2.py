da01c82c4753a624756e3be9cc6a331b4c6b10f9
a.saiba <a.saiba@alustudent.com>
2023-11-07 08:01:33 -0800

    ff

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+def divisible_by_2(my_list=[]):
+    temp_list = list()
+    for temp in my_list:
+        if temp % 2 == 0:
+            temp_list.append(True)
+        else:
+            temp_list.append(False)
+    return temp_list
